a0310432633dda2e9049728d3a3f5d0fa0112da7
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2025-07-01 22:57:33 -0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use.express.json();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
